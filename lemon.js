6fbe8ac8f0cc0077d880c1efaa6f0d95c9928887
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-10-07 01:00:22 +0200

    Lemons initial commit on the epic kyckling once game

---
+++ lemon.js
@@ -0,0 +1,27 @@
+setInterval(gameLoop, 1000/60)
+var counter = 0
+
+function gameLoop() {
+    counter++
+    if(counter % 60 === 0) {
+        console.log("output",counter    )
+    }
+    
+
+}
+
+window.addEventListener("keydown",keyListener)
+
+function keyListener(e) {
+    if(e.keyCode===32) {
+        console.log("32 pressed",counter % 60,rateMe(counter % 60))
+
+    }
+}
+
+function rateMe(timing) {
+    if((timing>55 && timing<=59) || (timing>=0 && timing<=3)) {
+        return true
+    }
+    return false
+}
